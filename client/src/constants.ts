772b1bb44d0b259c3594fb103fabea2d35d142f2
Olumide Adenigba <olumideadenigba@gmail.com>
2026-04-13 13:31:38 +0100

    chore: finale

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,5 +1,5 @@
 export const SERVER_URL =
   (import.meta as unknown as { env: Record<string, string> }).env
-    .VITE_SERVER_URL ?? "http://localhost:4021";
+    .VITE_SERVER_URL ?? "https://erebus.up.railway.app";
 
 export const EXPLORER_BASE = "https://testnet.stellarchain.io/transactions";
